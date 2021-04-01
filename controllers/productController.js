0826e81a4692c02a25f98268933f54673cde4d50
Mihail <mishobekama@abv.bg>
2021-04-01 16:14:38 +0300

    Add details Page

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
-const { Router } = require('express')
+const { Router } = require('express');
+const { route } = require('./homeController');
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -8,5 +9,11 @@ router.get('/', (req, res) => {
 router.get('/create', (req, res) => {
     res.render('create', { title: 'Create' });
 });
+
+router.get('/details/:productId', (req, res) => {
+    console.log(req.params.productId);
+
+    res.render('details', { title: 'ProductDetails' });
+})
 
 module.exports = router;
